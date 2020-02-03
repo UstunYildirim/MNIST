547c8d89e4f273fac46893555d6b2e1656d100bc
Ustun Yildirim <ustun@mailbox.org>
2020-02-03 11:22:28 -0500

    Weighted voting and returnVotes options added

--- kNN.py
+++ kNN.py
@@ -9,52 +9,64 @@ class kNN():
         s.Y = Y
         s.k = None
 
-    def optimizeK(s, Xtest, Ytest, kValues):
+    def optimizeK(s, Xtest, Ytest, kValues, withWeight=False):
         accs = []
         for k in kValues:
-            preds = np.array(s.predictMany(Xtest, k))
+            preds = np.array(s.predictMany(Xtest, k, withWeight))
             acc = np.sum(preds == Ytest)/Ytest.shape[0]
             accs.append(acc)
         i = np.argmax(accs)
         return (kValues[i], accs)
 
-    def errors(s, Xtest, Ytest, k):
+    def errors(s, Xtest, Ytest, k, withWeight=False):
         m = Xtest.shape[0]
-        boolMask = Ytest != [s.predict(Xtest[i], k) for i in range(m)]
+        preds = s.predictMany(Xtest, k, withWeight)
+        boolMask = Ytest != preds
         return Xtest[boolMask]
 
-    def predictMany(s, Xs, k):
+    def predictMany(s, Xs, k, withWeight=False):
         m = Xs.shape[0]
-        return [s.predict(Xs[i], k) for i in range(m)]
+        return [s.predict(Xs[i], k, withWeight) for i in range(m)]
 
-    def predict(s, X, k):
+    def predict(s, X, k, withWeight=False, returnVotes=False):
         m = s.Y.shape[0]
-        kMinNorms = []
+        kMinDists = []
         kVotes = []
         for i in range(m):
             nrm = np.linalg.norm(X - s.X[i,:])
-            for j in range(len(kMinNorms)):
-                if nrm < kMinNorms[j]:
-                    kMinNorms.insert(j, nrm)
+            for j in range(len(kMinDists)):
+                if nrm < kMinDists[j]:
+                    kMinDists.insert(j, nrm)
                     kVotes.insert(j,s.Y[i])
                     break
-            if len(kMinNorms) < k:
-                kMinNorms.append(nrm)
+            if len(kMinDists) < k:
+                kMinDists.append(nrm)
                 kVotes.append(s.Y[i])
-            elif len(kMinNorms) > k:
-                kMinNorms.pop()
+            elif len(kMinDists) > k:
+                kMinDists.pop()
                 kVotes.pop()
 
         count = {}
-        for v in kVotes:
-            if v in count:
-                count[v] = count[v] + 1
-            else:
-                count[v] = 1
+        if withWeight:
+            numVotes = len(kVotes)
+            for i,v in enumerate(kVotes):
+                if v in count:
+                    count[v] = count[v] + numVotes-i
+                else:
+                    count[v] = numVotes-i
+        else:
+            for v in kVotes:
+                if v in count:
+                    count[v] = count[v] + 1
+                else:
+                    count[v] = 1
+
 
         maxV = 0
-        for k,n in reversed(count.items()):
+        for k,n in count.items():
             if n > maxV:
                 maxV = n
                 selectedClass = k
+        if returnVotes:
+            return (k, count)
         return k
